999b90e73db4bc6f83799e545e07d0732ae81a97
rock-biter <74297920+rock-biter@users.noreply.github.com>
2025-07-07 14:14:17 +0200

    clean up

--- src/main.js
+++ src/main.js
@@ -1,28 +1,7 @@
 import './style.css'
 import 'lenis/dist/lenis.css'
 import * as THREE from 'three'
-// __controls_import__
-// __gui_import__
-
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { Pane } from 'tweakpane'
-
-/**
- * Debug
- */
-// __gui__
-// const config = {
-// 	example: 5,
-// }
-// const pane = new Pane()
-
-// pane
-// 	.addBinding(config, 'example', {
-// 		min: 0,
-// 		max: 10,
-// 		step: 0.1,
-// 	})
-// 	.on('change', (ev) => console.log(ev.value))
 
 /**
  * Scene
@@ -30,7 +9,6 @@ import { Pane } from 'tweakpane'
 const scene = new THREE.Scene()
 // scene.background = new THREE.Color(0xdedede)
 
-// __box__
 /**
  * BOX
  */
@@ -43,16 +21,6 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const mesh = new THREE.Mesh(geometry, material)
 mesh.position.y += 0.5
 scene.add(mesh)
-
-// __floor__
-/**
- * Plane
- */
-// const groundMaterial = new THREE.MeshStandardMaterial({ color: 'lightgray' })
-// const groundGeometry = new THREE.PlaneGeometry(10, 10)
-// groundGeometry.rotateX(-Math.PI * 0.5)
-// const ground = new THREE.Mesh(groundGeometry, groundMaterial)
-// scene.add(ground)
 
 /**
  * render sizes
@@ -71,15 +39,6 @@ camera.position.set(2, 2, 2)
 camera.lookAt(new THREE.Vector3(0, 2.5, 0))
 
 /**
- * Show the axes of coordinates system
- */
-// __helper_axes__
-// const axesHelper = new THREE.AxesHelper(3)
-// scene.add(axesHelper)
-
-// scene.background = new THREE.Color('lightgray')
-
-/**
  * renderer
  */
 const renderer = new THREE.WebGLRenderer({
@@ -91,7 +50,6 @@ handleResize()
 /**
  * OrbitControls
  */
-// __controls__
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
@@ -104,25 +62,9 @@ directionalLight.position.set(3, 10, 7)
 scene.add(ambientLight, directionalLight)
 
 /**
- * Three js Clock
- */
-// __clock__
-// const clock = new THREE.Clock()
-
-/**
  * frame loop
  */
 function tic() {
-	/**
-	 * tempo trascorso dal frame precedente
-	 */
-	// const deltaTime = clock.getDelta()
-	/**
-	 * tempo totale trascorso dall'inizio
-	 */
-	// const time = clock.getElapsedTime()
-
-	// __controls_update__
 	controls.update()
 
 	renderer.render(scene, camera)
